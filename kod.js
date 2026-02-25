0b0a074c3cbadaf2e7813cc8c23837c81fa1ee0f
maszlaiferencdonato <162970541+maszlaiferencdonato@users.noreply.github.com>
2026-02-25 14:13:05 +0100

    Add files via upload

--- kod.js
+++ kod.js
@@ -1,7 +1,9 @@
 function nagyitKep(melyikKep) {
+    document.getElementById("nagykep").src = melyikKep;
 }
 
 function visszaKep() {
+    document.getElementById("nagykep").src = "ures.jpg";
 }
 
 function kivalaszt(melyikTermek) {
